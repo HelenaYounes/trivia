f8b38938ab662aab5c9a78a3a33c040d93e9dbfc
Héléna Younes <younesq.helena@gmail.com>
2017-11-20 23:28:25 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Button, Card, Icon, Layout, Menu } from 'antd';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import { Router, Route, Switch  } from 'react-router';
+import { SocialIcon } from 'react-social-icons';
+import Home from './Home.js'
+import Contact from './Contact.js'
+import 'antd/dist/antd.css';
+import './App.css';
+const { Header, Content, Footer } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Header>
+          <div className='App-header'>
+            <div className="App_menu">
+              <Button type="primary" style={{ marginBottom: 16 }}>
+                <Icon type='menu-unfold' />
+              </Button>
+              <Menu
+                mode="inline"
+                theme="dark"
+                inlineCollapsed='false'
+              >
+                <Menu.Item key="1">
+                  <span>Contact</span>
+                </Menu.Item>
+              </Menu>
+            </div>
+            <h1 className="App-title" style={{color:'white'}}>Some Title</h1>
+            <div className="social-media-icons">
+              <SocialIcon url="http://twitter.com"/>
+              <SocialIcon url="http://facebook.com"/>
+              <SocialIcon url="http://linkedin.com"/>
+            </div>
+          </div>
+
+        </Header>
+        <Content>
+          <Router>
+            <Switch>
+              <Route path='/' component={Home} />
+            </Switch>
+          </Router>
+          <Router>
+            <Switch>
+              <Route exact path='/contact' component={Contact} />
+            </Switch>
+          </Router>
+        </Content>
+      </Layout>
+    );
+  }
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Card } from 'antd';
+import 'antd/dist/antd.css';
+import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+    <div className='card'>
+        <Card title="Card title" style={{ width: 300}}>
+         <p>Card content</p>
+        </Card>
+    </div>
+    );
+  }
+}
+
+export default Home;
