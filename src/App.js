06a676d1113aed5bc53f424a8af2156166f9963c
Héléna Younes <younesq.helena@gmail.com>
2017-12-15 18:49:27 -0800

    fix formatting

--- src/App.js
+++ src/App.js
@@ -11,24 +11,24 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <Layout>
-        <Header>
-          <div className='App-header'>
-            <Icon type="bars" style={{fontSize: 20, color:'white'}}/>
-            <Link to='/'><h1 className="App-title" style={{color:'white'}}>Trivia</h1></Link>
-            <Icon type="qq" style={{fontSize: 20, color:'white'}}/>
-          </div>
-        </Header>
-        <Content>
+        <Layout>
+          <Header>
+            <div className='App-header'>
+              <Icon type="bars" style={{fontSize: 20, color:'white'}}/>
+              <Link to='/'><h1 className="App-title" style={{color:'white'}}>Trivia</h1></Link>
+              <Icon type="qq" style={{fontSize: 20, color:'white'}}/>
+            </div>
+          </Header>
+          <Content>
             <div className='App-content' >
               <Switch>
                 <Route path='/quizzes/:quizId/questions/:id' component={Quiz} />
                 <Route path='/' component={Home} />
               </Switch>
             </div>
-        </Content>
-      </Layout>
-    </Router>
+          </Content>
+        </Layout>
+      </Router>
     );
   }
 }
